78cf4a4c46d0ad6bcf628708fdd7c7c9ff35a15c
Charles Lehman <lehmanc24@gmail.com>
2022-02-02 23:23:37 -0800

    Added Enemy Behavior

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float _speed = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move down at 4 meters a second
+        //if at the bottom of the screen respawn at the top in a random x position
+
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        //pos at top is 7.44
+        //pos at bottom is -5.38
+        //far left -8.48 far right 8.45
+        if (transform.position.y < -5.38)
+        {
+            transform.position = new Vector3(Random.Range(-8.48f, 8.45f), 7.44f, 0);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,34 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // [SerializeField] private float _speed = 2.2f;
-    //private int _playerLives = 3;
     [SerializeField] private float _speed = 11.45f;
     public GameObject bullet;
     [SerializeField]
     private float fireDelay;
     [SerializeField]
     private float fireRate = .3f;
-
-    // public bool gameOver = false;
-
-    // public string playerName = "Clara";
-    // Start is called before the first frame update
     void Start()
     {
-        //Set our players position to 0,0,0 when the game starts.
         transform.position = new Vector3(0, 0, 0);
     }
-
-    // Update is called once per frame
     void Update()
     {
         Movement();
         if (Input.GetKey("space") && Time.time > fireDelay)
         {
-            fireDelay = Time.time + fireRate;
-
-            Instantiate(bullet, new Vector3(transform.position.x, transform.position.y + 1.05f, 0), Quaternion.identity);
+            ShootBullet();
         }
     }
     void Movement()
@@ -42,36 +30,26 @@ public class Player : MonoBehaviour
         transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
         transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
 
-
-        //if the position of the player y is > 6
-        //we make the players y 6
-        //else if the position of the player y is <-4
-        //player position is -4
-
         if (transform.position.y > 6)
         {
             transform.position = new Vector3(transform.position.x, 6, 0);
-
         }
         else if (transform.position.y < -4)
         {
             transform.position = new Vector3(transform.position.x, -4, 0);
         }
-
-        //if the position of the players x is < -10.19612 
-        //set the position to 10.15831
-        //else if the player x position is > 10.15831
-        //set the position to -10.19612
-
         if (transform.position.x < -10.19612)
-
         {
             transform.position = new Vector3(10.15831f, transform.position.y, 0);
-
         }
         else if (transform.position.x > 10.15831)
         {
             transform.position = new Vector3(-10.19612f, transform.position.y, 0);
         }
+    }
+    void ShootBullet()
+    {
+        fireDelay = Time.time + fireRate;
+        Instantiate(bullet, new Vector3(transform.position.x, transform.position.y + 1.05f, 0), Quaternion.identity);
     }
 }
